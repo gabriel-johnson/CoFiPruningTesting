0b4093f473e19720c2686da5f0cc066639755bea
Gabriel Johnson <johnsga2@oregonstate.edu>
2025-02-14 17:20:24 -0500

    script update

--- sbatch_scripts/batch_jobs_mrpc_qnli_qqp_50.sh
+++ sbatch_scripts/batch_jobs_mrpc_qnli_qqp_50.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -J MRPC_QNLI_QQP_TESTING_50                                               
+#SBATCH -J MRPC+3Epochs                                               
 #SBATCH -A eecs   
 #SBATCH -p dgx2                                                           
 #SBATCH -e out/mrpc_qnli_qqp_50_err                            
 #SBATCH -o out/mrpc_qnli_qqp_50_out                            
 
-#SBATCH --time=7-00:00:00                 
+#SBATCH --time=3-00:00:00                 
 #SBATCH --gres=gpu:1                         
 #SBATCH --mem=128G                          
 
